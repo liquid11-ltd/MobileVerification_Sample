d324f64793edf8180ebcdc29e438a7c8fd1c76af
Mike Knights <mk@mkdev.co.uk>
2016-10-24 15:01:32 +0100

    main sample code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Liquid11_MobileVerification;
+using Newtonsoft.Json;
+
+namespace Liquid11_MobileVerification_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an account and set an api password at https://www.mobileverification.co.uk
+            using (var client = new Client("myuser@mydomain.com", "mypassword"))
+            {
+                Console.WriteLine("# Tps Request");
+                Console.WriteLine(JsonConvert.SerializeObject(client.TpsLookup("0044***********").Result, Formatting.Indented));
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("# Hlr Request");
+                Console.WriteLine(JsonConvert.SerializeObject(client.HlrLookup("0044***********").Result, Formatting.Indented));
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("# Landline Request");
+                Console.WriteLine(JsonConvert.SerializeObject(client.LandlandLookup("0044***********").Result, Formatting.Indented));
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("# Email");
+                Console.WriteLine(JsonConvert.SerializeObject(client.EmailLookup("myuser@mydomain.com").Result,Formatting.Indented));
+                Console.WriteLine("\n\n");
+            }
+
+            Console.WriteLine("\n\n\n All done!");
+            Console.Read();
+        }
+    }
+}
